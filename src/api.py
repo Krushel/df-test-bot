07d3ccc6a130143f4956853ca910b63fa8e4befb
Krushel <72271923+Krushel@users.noreply.github.com>
2024-02-27 17:50:36 +0300

    Reworked api.py
    Made so api.py sends only the useful info to sqs queue

--- src/api.py
+++ src/api.py
@@ -13,8 +13,12 @@ queue = sqs.Queue(SQS_QUEUE_URL)
 
 def lambda_handler(event, context):
     logger.info("event: {}".format(json.dumps(event)))
-    try:    
-        queue.send_message(MessageBody=event["body"])
+    try:
+        # I`m getting rid of all the unnecessary data from telegram and sending only the message and chat_id
+        message_body = json.loads(event["body"])
+        message_to_queue = json.dumps({"message": message_body['message']['text'],
+                                       "chat_id": message_body['message']['chat']['id']})
+        queue.send_message(MessageBody=message_to_queue)
         return {
             'statusCode': 200,
             'body': 'Success'
